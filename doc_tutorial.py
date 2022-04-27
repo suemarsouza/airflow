f118d7f4681a2af041e0f9c78c2e7781febfcccf
suemarsouza <suemar.souza@bairesdev.com>
2022-04-26 22:20:38 -0300

    Tuto First Part

--- doc_tutorial.py
+++ doc_tutorial.py
@@ -1,2 +1,85 @@
 #https://airflow.apache.org/docs/apache-airflow/stable/tutorial.html
 
+from datatime import datetime, timedelta
+from textwrap import dedent
+
+# The DAG object? we`ll need this to instantiate a DAG
+
+from airflow import DAG
+# Operators
+from airflow.operators.bash import BashOperator
+with DAG(
+    'tutorial',
+    #These args will get passed on to each operator
+    #It's possible to override them on per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': 'some_email@domain.com',
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function,
+        # 'on_success_callback': some_other_function,
+        # 'on_retry_callback': another_function,
+        # 'sla_miss_callback': yet_another_function,
+        # 'trigger_rule': 'all_success'
+    },
+    description='Tutorial DAG',
+    schedule_interval=timedelta(days=1), #@daily
+    start_date=datetime(2021,1,1),
+    catchup=False,
+    tags=['example', 'tuto', '1st']
+) as dag:
+
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+    )
+
+    t2 = BashOperator(
+        task_id='sleep',
+        depends_on_past=False,
+        bash_command='sleep 5',
+        retries=3
+    )
+
+    t1.doc_md = dedent(
+        """\
+    #### Task Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+
+    """
+    )
+
+    dag.doc_md = __doc__ # providing that you have a docstring at the beginning of the DAG
+    dag.doc_md = """
+    This is a documentation placed anywhere
+    """  # otherwise, type it like this
+    templated_command = dedent(
+        """
+            {% for i in range(5) %}
+                echo "{{ ds }}"
+                echo "{{ macros.ds_add(ds, 7)}}"
+            {% endfor %}
+            """
+    )
+
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command=templated_command,
+    )
+
+    t1 >> [t2, t3]
